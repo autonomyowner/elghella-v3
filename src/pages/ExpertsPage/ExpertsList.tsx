5304878e65e11daeea5c72586c6aeac432dc18b3
autonomyowner <autonomy.owner@gmail.com>
2025-06-30 23:40:26 +0100

    Fix: remove unused React import to fix Vercel build

--- src/pages/ExpertsPage/ExpertsList.tsx
+++ src/pages/ExpertsPage/ExpertsList.tsx
@@ -1,5 +1,5 @@
 import { Link } from "react-router-dom";
-import React, { useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 import { getExpertsApi } from "../../api/getExpertsApi";
 
 export default function ExpertsList() {
